627b0671898f5dada4776012c1c11909cf9eabf3
Anton <antonfiveis@gmail.com>
2020-05-28 12:49:32 +0300

    init with prototypes

---
+++ LR6.1/LR6.1.cpp
@@ -0,0 +1,14 @@
+﻿#include "figure_in_space.h"
+using namespace std;
+int main()
+{
+    half_piramide h;
+    cout << "half piramide" << endl;
+    cout <<"V:"<< h.volume()<<endl<<"Full square:"<<h.full_square()<<endl;
+    piramide pir(2.5, { 1, 2, 1.5, 3, 3, 3 });
+    cout << "piramide" << endl;
+    cout << "V:" << pir.volume() << endl << "Full square:" << pir.full_square()<<endl;
+    prisma pr(2.5, { 1, 2, 1.5, 3 });
+    cout << "prisma" << endl;
+    cout << "V:" << pr.volume() << endl << "Full square:" << pr.full_square()<<endl;
+}
